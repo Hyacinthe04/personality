88871f99e881db609f9015d0427de840da24f1e9
UMULISA12 <umulisaa0@gmail.com>
2019-01-16 15:12:49 +0200

    html

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,98 @@
 
- // Business logic
+//  Business logic
 
  function noAnswer(){
         
-    if (!$('[name=qtn1]').is(':checked') || !$('[name=qtn2]').is(':checked')){
+    if (!$('[name=Q1]').is(':checked') || !$('[name=Q2]').is(':checked') || !$('[name=Q3]').is(':checked') || !$('[name=Q4]').is(':checked') || !$('[name=Q5]').is(':checked') || !$('[name=Q6]').is(':checked')){
 
         return true;
 
     }       
 
-}
+};
 var openness=0;
 var introversion=0;
 var conscientiousness=0;
 var agreeableness=0;
 var extroversion=0;
 var neuroticism=0;
+    
 
+// function answersChecked(questions, answers){
 
-function answersChecked(questions, answers){
+//     if (questions === answers){
+//         return true;
+//     }
 
-    if (questions === answers){
-        return true;
-    }
-
-}
+// }
 
 // User interface logic
 
 $(document).ready(function() {
 
 
-$("#button").submit(function(event) {
+$("form#options").submit(function(event) {
 
-var Q1 = $("input:radio[name='qtn1']:checked").val();
-var Q2 = $("input:radio[name='qtn2']:checked").val();
+var buttonOne = $("input:radio[name='Q1']:checked").val();
+var buttonTwo= $("input:radio[name='Q2']:checked").val();
+var buttonThree = $("input:radio[name='Q3']:checked").val();
+var buttonFour= $("input:radio[name='Q4']:checked").val();
+var buttonFive = $("input:radio[name='Q5]:checked").val();
+var buttonSix= $("input:radio[name='Q6']:checked").val();
 
 if(noAnswer())
 {
     alert("please answer all questions!");
 }
+event.preventDefault();
+});
+});
 
-else{
+// else{
 
     
-    if (answersChecked(Q1, "nganira n'inshuti zanjye gusa")) {
-        introversion += 10;
-    }
+//     if (answersChecked(Q1, "nganira n'inshuti zanjye gusa")) {
+//         introversion += 10;
+//     }
 
-    else if (answersChecked(Q1, "nganira n'abantu tumenyeranye")) {
-        extroversion += 10;
-    }
+//     else if (answersChecked(Q1, "nganira n'abantu tumenyeranye")) {
+//         extroversion += 10;
+//     }
 
-    else if (answersChecked(Q1, "nganira n'abantu bose")) {
-        open += 10;
-    }
-    else {
-         agreeableness += 10;
-    }
+//     else if (answersChecked(Q1, "nganira n'abantu bose")) {
+//         open += 10;
+//     }
+//     else {
+//          agreeableness += 10;
+//     }
 
   
-//     $("#quiz").hide();
-//     $("#marks").text("Your score is: "+ total+ "/90");
-//     $("#results").show();
- alert("you have selected" +answersChecked);
- }
-  event.preventDefault();
-});
+// //     $("#quiz").hide();
+// //     $("#marks").text("Your score is: "+ total+ "/90");
+// //     $("#results").show();
+//  alert("you have selected" +answersChecked);
+//  }
+//   event.preventDefault();
+// });
 
-});
+// });
+
+
+
+// function myFunction() {
+//     if (document.getElementById('a').checked) {
+//       document.getElementById("fight").style.display= 'block';
+//       document.getElementById("comeback").style.display= 'none';
+//       document.getElementById("run").style.display= 'none';
+  
+//     }else if (document.getElementById('b').checked) {
+//       document.getElementById("comeback").style.display= 'block';
+//       document.getElementById("fight").style.display= 'none';
+//       document.getElementById("run").style.display= 'none';
+//     }else if (document.getElementById('c').checked) {
+//       document.getElementById("run").style.display= 'block';
+//       document.getElementById("fight").style.display= 'none';
+//       document.getElementById("comeback").style.display= 'none';
+//     }
+  
+  
